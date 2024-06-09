b284d3dde078e780b8e5124cb65ce55f79472c91
oksanaprokopenko <oksana.prokopenko16@gmail.com>
2024-06-10 02:49:23 +0300

    fix name

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -10,7 +10,7 @@ def test_post_v1_account():
     account_api = AccountApi(host='http://5.63.153.31:5051')
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
-    login = 'optees4984t63'
+    login = 'optees4984t64'
     password = '12345as'
     email = f'{login}@mail.ru'
 
