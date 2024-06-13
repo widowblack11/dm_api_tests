dccff37b06368a31ea65c957ea107d5d6d38f8b3
oksanaprokopenko <oksana.prokopenko16@gmail.com>
2024-06-13 22:45:26 +0300

    update  tests

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -26,7 +26,7 @@ def test_post_v1_account():
     mailhog = MailHogApi(configuration=mailhog_configuration)
     account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog)
 
-    login = '8o11'
+    login = '8oe11'
     password = '123345as8'
     email = f'{login}@mail.ru'
 
--- tests/functional/put_v1_account_email/test_put_v1_account_email.py
+++ tests/functional/put_v1_account_email/test_put_v1_account_email.py
@@ -30,7 +30,7 @@ def test_put_v1_account_email():
     mailhog = MailHogApi(configuration=mailhog_configuration)
     account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog)
 
-    login = '8o6'
+    login = '8o6e'
     password = '123345as8'
     email = f'{login}@mail.ru'
     new_email=f'{login}new@mail.ru'
