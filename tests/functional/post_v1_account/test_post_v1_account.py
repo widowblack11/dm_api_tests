9db5cf0e0133af830429d895989023bf0a7bd87c
oksanaprokopenko <oksana.prokopenko16@gmail.com>
2024-06-13 22:28:29 +0300

    add facade and update test

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,17 +1,19 @@
-from dm_api_account.apis.account_api import AccountApi
-from dm_api_account.apis.login_api import LoginApi
-from api_mailhog.apis.mailhog_api import MailhogApi
 import structlog
+
+from helpers.account_helper import AccountHelper
+from services.api_mailhog import MailHogApi
 from restclient.configuration import Configuration as MailhogConfiguration
 from restclient.configuration import Configuration as DmApiConfiguration
+from services.dm_api_account import DMApiAccount
+
 structlog.configure(
     processors=
     [structlog.processors.JSONRenderer(
         indent=4,
         ensure_ascii=True,
-        #sort_keys=True
+        # sort_keys=True
     )
-     ]
+    ]
 )
 
 
@@ -19,42 +21,14 @@ def test_post_v1_account():
     # Регистрация пользователя
     mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
     dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
-    account_api = AccountApi(configuration=dm_api_configuration)
-    login_api = LoginApi(configuration=dm_api_configuration)
-    mailhog_api = MailhogApi(configuration=mailhog_configuration)
-    login = '8opt3eesfggggg67'
+
+    account = DMApiAccount(configuration=dm_api_configuration)
+    mailhog = MailHogApi(configuration=mailhog_configuration)
+    account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog)
+
+    login = '8o3pt3eesdgg3egggg67'
     password = '123345as8'
     email = f'{login}@mail.ru'
 
-    json_data = {
-        'login': login,
-        'email': email,
-        'password': password
-    }
-
-    response = account_api.post_v1_account(json_data=json_data)
-
-    assert response.status_code == 201, f'Новый пользователь не был создан, {response.json()}'
-    # Получить письма из почтового сервера
-
-    response = mailhog_api.get_api_v2_messages()
-
-    assert response.status_code == 200, 'Письма не были получены'
-    # Получить активационный токен
-    token = mailhog_api.get_activate_token_by_login(login, email, response)
-    assert token is not None, f'Токен для пользователя {login} не был получен'
-
-    # активация пользователя
-    response = account_api.put_v1_account_to_token(token=token)
-
-    assert response.status_code == 200, 'Пользователь не был активирован'
-    # авторизация
-    json_data = {
-        'login': login,
-        'password': password,
-        'rememberMe': True
-    }
-
-    response = login_api.post_v1_account_login(json_data=json_data)
-
-    assert response.status_code == 200, 'Пользователь не смог авторизоваться'
+    account_helper.register_new_user(login=login, password=password, email=email)
+    account_helper.user_login(login=login, password=password)
