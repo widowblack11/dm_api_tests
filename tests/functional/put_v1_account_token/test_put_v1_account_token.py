0f8b5c68b78510adc5ed9e747b98921a2ad40227
oksanaprokopenko <oksana.prokopenko16@gmail.com>
2024-06-13 19:45:38 +0300

    fix

--- tests/functional/put_v1_account_token/test_put_v1_account_token.py
+++ tests/functional/put_v1_account_token/test_put_v1_account_token.py
@@ -10,7 +10,7 @@ def test_put_v1_account_token():
     account_api = AccountApi(host='http://5.63.153.31:5051')
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
-    login = 'd'
+    login = 'd56'
     password = '1263345as'
     email = f'{login}@mail.ru'
 
