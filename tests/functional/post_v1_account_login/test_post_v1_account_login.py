35efb9f25e5d39b5df9a4d32651273535513ba82
oksanaprokopenko <oksana.prokopenko16@gmail.com>
2024-06-20 23:26:04 +0300

    fix imports

--- tests/functional/post_v1_account_login/test_post_v1_account_login.py
+++ tests/functional/post_v1_account_login/test_post_v1_account_login.py
@@ -1,27 +1,3 @@
-from json import loads
-
-import structlog
-
-from dm_api_account.apis.account_api import AccountApi
-from dm_api_account.apis.login_api import LoginApi
-from api_mailhog.apis.mailhog_api import MailhogApi
-from helpers.account_helper import AccountHelper
-from restclient.configuration import Configuration as MailhogConfiguration
-from restclient.configuration import Configuration as DmApiConfiguration
-from services.api_mailhog import MailHogApi
-from services.dm_api_account import DMApiAccount
-
-structlog.configure(
-    processors=
-    [structlog.processors.JSONRenderer(
-        indent=4,
-        ensure_ascii=True,
-        # sort_keys=True
-    )
-    ]
-)
-
-
 def test_post_v1_account_login(
         account_helper,
         prepare_user
