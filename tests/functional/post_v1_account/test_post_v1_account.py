b8eaa7b92c8370d5153463da44610951cb595055
oksanaprokopenko <oksana.prokopenko16@gmail.com>
2024-06-28 16:21:57 +0300

    add hamcrest asserts

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,3 +1,17 @@
+from datetime import datetime
+
+from hamcrest import (
+    assert_that,
+    has_property,
+    starts_with,
+    all_of,
+    instance_of,
+    has_properties,
+    equal_to,
+
+)
+
+
 def test_post_v1_account(
         account_helper,
         prepare_user
@@ -6,4 +20,25 @@ def test_post_v1_account(
     password = prepare_user.password
     email = prepare_user.email
     account_helper.register_new_user(login=login, password=password, email=email)
-    account_helper.user_login(login=login, password=password)
+    response = account_helper.user_login(login=login, password=password, validation_response=True)
+    assert_that(
+        response, all_of(
+            has_property('resource', has_property('login', starts_with("prokopenko"))),
+            has_property('resource', has_property('registration', instance_of(datetime))),
+            has_property(
+                'resource', has_properties(
+                    {
+                        'rating': has_properties(
+                            {
+                                "enabled": equal_to(True),
+                                "quality": equal_to(0),
+                                "quantity": equal_to(0)
+                            }
+                        )
+                    }
+                )
+            )
+        )
+    )
+
+    print(response)
